cd0ecf6477da28245e3e5f2f4663a53d4124419d
Guy Dvorkin <dvorkin.guy@gmail.com>
2023-12-22 21:06:51 +0200

    day 3

---
+++ 3_rollercoaster_game/love_calculator.py
@@ -0,0 +1,10 @@
+print("Type your Full Names one by one\n")
+name1 = input() # What is your name?
+name2 = input() # What is their name?
+print("\nThe Love Calculator is calculating your score...\n")
+
+t = 0
+
+if "t" in name1 and name2:
+    t += 1
+print(t)
